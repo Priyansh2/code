761bb3072787b6f3e8d7db27d01e8e6d23274f88
Priyansh Agrawal <priyanshagarwal02may@gmail.com>
2021-09-15 20:57:46 +0530

    Time: 200 ms (85.58%), Space: 79.2 MB (46.93%) - LeetHub

--- maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,3 +1,15 @@
+class node{
+    public:
+    
+    node *left;
+    node * right;
+    int val;
+    node(int val= -1){
+        left = nullptr;
+        right = nullptr;
+        this->val = val;
+    }
+};
 class Solution {
 public:
     int solve1(vector<int> &nums){
@@ -23,8 +35,52 @@ public:
         return maxxor;
         
     }
+    void insert(int n, int k, node *root){
+        if(k<0) return;
+        int bit = (n >> k) & 1;
+        if(bit==0){
+            if(!root->left) root->left = new node(bit);
+            root = root->left;
+        }
+        else{
+            if(!root->right) root->right = new node(bit);
+            root =root->right;
+        }
+        insert(n,k-1,root);
+    }
+    
+    int solve2(vector<int> &nums){
+        node * root=  new node();
+        for(auto x : nums){
+            insert(x,30,root);
+        }
+        int res = 0;
+        for(auto x : nums){
+            int maxxor = 0;
+            auto t = root;
+            for(int i = 30;i>=0;i--){
+                int bit =(x >> i) & 1;
+                if(bit==0){
+                    if(t->right){
+                        t =t->right;
+                        maxxor+= 1 << i;
+                    }
+                    else t = t->left;
+                }
+                else{
+                    if(t->left){
+                        t = t->left;
+                        maxxor+= 1 << i;
+                    }
+                    else t =t->right;
+                }
+            }
+            res = max(res,maxxor);
+        }
+        return res;
+    }
     int findMaximumXOR(vector<int>& nums) {
-        return solve1(nums);
-        //return solve2(nums);
+        //return solve1(nums);
+        return solve2(nums);
     }
 };
