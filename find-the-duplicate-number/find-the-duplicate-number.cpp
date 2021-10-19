1576f28e8bf25291b998697331e31f79edcd14bf
Priyansh Agrawal <priyanshagarwal02may@gmail.com>
2021-10-19 15:55:37 +0530

    Time: 151 ms (41.57%), Space: 61.3 MB (60.00%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -10,9 +10,17 @@ public:
         return -1;
     }
     int solve2(vector<int> &nums){
-        return 0;
+        int n = nums.size();
+        for(auto &x : nums) x--;
+        for(auto &x : nums){
+            nums[x%n]+=n;
+            if(nums[x%n] >= 2*n) return x%n+1;
+        }
+        return -1;
+        
     }
     int findDuplicate(vector<int>& nums) {
-        return solve1(nums);
+        //return solve1(nums);
+        return solve2(nums);
     }
 };
