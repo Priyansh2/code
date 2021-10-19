46cfc07c3f0671afd418fac89b32a94ae4460744
Priyansh Agrawal <priyanshagarwal02may@gmail.com>
2021-10-19 16:21:22 +0530

    Time: 37 ms (15.89%), Space: 17.9 MB (93.66%) - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res = 0;
+        /*int res = 0;
         for(auto &x : nums) res^=x;
         for(int i =0;i<=nums.size();i++) res^=i;
-        return res;
+        return res;*/
+        sort(nums.begin(),nums.end());
+        int low = 0, high = nums.size();
+        int ans = -1;
+        while(low <=high){
+            int mid = low + (high - low)/2;
+            if(mid >= nums.size() || mid < 0 || nums[mid] > mid) {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid +1;
+        }
+        return ans;
     }
 };
