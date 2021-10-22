0b85e346fc5e4e1fb157ffe22fb90a292f6084a2
Priyansh Agrawal <priyanshagarwal02may@gmail.com>
2021-10-22 21:52:29 +0530

    Time: 254 ms (73.06%), Space: 55.3 MB (57.58%) - LeetHub

--- find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -9,7 +9,43 @@ public:
         sort(nums.begin(),nums.end(),compare);
         return nums[k-1];
     }
+    int partition(vector<string> &nums, int l, int r){
+        int n = rand()%(r-l+1);
+        swap(nums[l+n],nums[r]);
+        string pivot = nums[r];
+        int i=l;
+        n = nums.size();
+        for(int j =l;j<r;j++){
+            if(compare(nums[j],pivot)){
+                swap(nums[i++],nums[j]);
+            }
+        }
+        swap(nums[i],nums[r]);
+        return i;
+    }
+    string solve(vector<string> &nums, int l, int r, int k){
+        /*if(l>r) return "";
+        int p = partition(nums,l,r);
+        if(p==k-1)return nums[p];
+        return k-1 < p ? solve(nums,l,p-1,k) : solve(nums,p+1,r,k);*/
+        
+        /*while(l<=r){
+            int p =partition(nums,l,r);
+            if(p==k-1) return nums[p];
+            else if(k-1 < p) r = p-1;
+            else l = p+1;
+        }
+        return "";*/
+        nth_element(nums.begin(),nums.begin() + k-1 , nums.end(),compare);
+        return nums[k-1];
+    }
+    string solve2(vector<string>&nums, int k){
+        srand(time(0));
+        
+        return solve(nums,0,nums.size()-1,k);
+    }
     string kthLargestNumber(vector<string>& nums, int k) {
-        return solve1(nums,k);
+        //return solve1(nums,k);
+        return solve2(nums,k);
     }
 };
